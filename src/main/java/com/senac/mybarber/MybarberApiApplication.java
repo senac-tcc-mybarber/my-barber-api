158bea1262409d3f60de735719c2ba2cab8d1f7f
Lucas <lucasgoncalvesb@gmail.com>
2019-12-15 18:30:00 -0300

    Adiciona a estrutura basica do projeto com Spring Boot

---
+++ src/main/java/com/senac/mybarber/MybarberApiApplication.java
@@ -0,0 +1,13 @@
+package com.senac.mybarber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybarberApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybarberApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senac/mybarber/MybarberApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.mybarber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybarberApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
