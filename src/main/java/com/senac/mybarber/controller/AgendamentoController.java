14d8577b108363ca595e10e47b86c200e88451be
Rafael Cabral <rafaelmcabral@gmail.com>
2020-02-26 02:26:33 -0300

    Criando serviço de obtenção de agendamento selecionado.

---
+++ src/main/java/com/senac/mybarber/controller/AgendamentoController.java
@@ -0,0 +1,24 @@
+package com.senac.mybarber.controller;
+
+import com.senac.mybarber.service.AgendamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/agendamentos")
+public class AgendamentoController {
+
+    @Autowired
+    private AgendamentoService agendamentoService;
+
+    @GetMapping(path = {"/{id}"})
+    public ResponseEntity findById(@PathVariable Long id) {
+        return agendamentoService.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
